75a45d2403d49a69601494d8c85132c861983aab
paulacarvani <3312@holbertonschool.com>
2022-02-10 15:45:06 -0500

    Task2, First files, update files, js in construction

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+// Your script must be executed only when DOM is loaded
+$(document).ready(function(){
+    //if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
+    // creates a directory or list
+    const Selection_check = []
+
+    //agregamos data-name al final de la lista
+    Selection_check.push();
+
+    // if the checkbox is unchecked, you must remove the Amenity ID from the variable
+
+    // update the h4 tag inside the div Amenities with the list of Amenities checked
+
+
+
+    })
