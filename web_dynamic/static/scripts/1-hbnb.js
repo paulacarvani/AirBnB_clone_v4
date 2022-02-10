d92c3e7115d2aa84b707e7f1b91a8ee0bf019790
paulacarvani <3312@holbertonschool.com>
2022-02-10 18:08:42 -0500

    Update script for checkbox to amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,21 @@
 // Your script must be executed only when DOM is loaded
-$(document).ready(function(){
+$(document).ready(function() {
     //if the checkbox is checked, you must store the Amenity ID in a variable (dictionary or list)
     // creates a directory or list
     const Selection_check = []
-
-    //agregamos data-name al final de la lista
-    Selection_check.push();
-
-    // if the checkbox is unchecked, you must remove the Amenity ID from the variable
-
-    // update the h4 tag inside the div Amenities with the list of Amenities checked
-
-
-
-    })
+    // Listen for changes on each input checkbox tag:
+    $('input:checkbox').change(function(){
+        if ($(this).is(':checked')) {
+            //agregamos data-name al final de la lista
+            Selection_check.push($(this).parent().attr('data-name'));
+        } else {
+            // if the checkbox is unchecked, you must remove the Amenity ID from the variable
+            const index = Selection_check.indexOf(($(this).parent().attr('data-name')));
+            if (index > -1) {
+                Selection_check.splice(index, 1);
+            }
+        }
+        // update the h4 tag inside the div Amenities with the list of Amenities checked
+        $('.amenities h4').text(Selection_check.join('. '));
+    });
+});
